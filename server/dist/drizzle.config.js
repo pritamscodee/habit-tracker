ae2a0aeb69da79728cbc40f483651af0d986105e
pritamscodee <codewithpritam29@gmail.com>
2026-04-15 18:46:12 +0530

    delted unuseful files

--- server/dist/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    schema: "./src/db/schema.ts",
-    out: "./src/db/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-    },
-};
--- server/dist/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import create_habits, { delete_habits } from "../controller/habit-post";
-import { Get_Req } from "../controller/habit-get.controller";
-const router = Router();
-router.post("/post", create_habits);
-router.get("/get", Get_Req);
-router.delete('/del/:id', delete_habits);
-export { router };

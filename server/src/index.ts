78b60454c37726e5e1a76382e21f689d96ea00e6
pritamscodee <codewithpritam29@gmail.com>
2026-04-18 02:33:20 +0530

    cmit

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "https://habit-tracker-1ymkq5zxl-pritamscodees-projects.vercel.app/",
+    origin: "https://habit-tracker-1ymkq5zxl-pritamscodees-projects.vercel.app",
   }),
 );
 

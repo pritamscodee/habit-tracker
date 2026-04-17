178cfe63c29df9166dcb0931fe81f7576020cadb
pritamscodee <codewithpritam29@gmail.com>
2026-04-18 03:31:47 +0530

    netifly --

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,9 +10,7 @@ export const app = express();
 app.use(
   cors({
     origin: [
-      "https://habit-tracker-6r0w1kq3t-pritamscodees-projects.vercel.app",
-      "https://habit-tracker-git-main-pritamscodees-projects.vercel.app",
-      "https://habit-tracker-39w5su35b-pritamscodees-projects.vercel.app/"
+    "https://habiatio.netlify.app"
     ]
   })
 );
